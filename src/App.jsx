c3b82a8bf330fddf56007bec0080c78041a187b6
Md.Rokonuzzaman Sayem <sayem99133@gmail.com>
2024-03-14 06:47:40 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-purple-500">
+      <h1
+        className="text-center text-white p-2
+       text-4xl font-bold mb-2"
+      >
+        Course Registration
+      </h1>
+      <hr />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/Courses/Courses.jsx
@@ -0,0 +1,9 @@
+const Courses = () => {
+  return (
+    <div>
+      <h2>sayem</h2>
+    </div>
+  );
+};
+
+export default Courses;
